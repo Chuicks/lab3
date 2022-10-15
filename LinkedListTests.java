73049cea77a595c333a28c18fe501b8938e3e8f2
Chuicks <eyzhang@ucsd.edu>
2022-10-14 21:14:23 -0700

    bruh

--- LinkedListTests.java
+++ LinkedListTests.java
@@ -9,9 +9,8 @@ public void testprepend() {
        
         input1.append(4);
         input1.append(6);
+        input1.append(6);
         assertEquals(1, input1.length());
     }
 }
 
-//        LinkedList input1 = new LinkedList();
-//        Noder input2 =  new Node(4, input3);
